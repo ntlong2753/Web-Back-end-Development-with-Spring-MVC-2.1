1f7b8f2ca942d22ae755c8610ed2dd24a74ed5ae
ntlong2753 <nguyenthanhlongvn03@gmail.com>
2026-05-26 15:59:31 +0700

    add

--- Demo/demoxxx/src/main/java/com/codegym/demoxxx/controller/UserController.java
+++ Demo/demoxxx/src/main/java/com/codegym/demoxxx/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.codegym.demoxxx.controller;
 
 import com.codegym.demoxxx.entities.User;
+import com.codegym.demoxxx.request.UserRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +31,29 @@ public class UserController {
 
     @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") int id, Model model) {
-        System.out.println(id);
+        // 1. Tìm user có id trùng với id truyền vào
+        User userToDelete = null;
+        for (User user : listUser) {
+            if (user.getId() == id) {
+                userToDelete = user;
+                break; // Tìm thấy rồi thì dừng vòng lặp luôn
+            }
+        }
+        // 2. Nếu tìm thấy thì xóa khỏi danh sách
+        if (userToDelete != null) {
+            listUser.remove(userToDelete);
+        }
+        return "redirect:/users";
+    }
+
+    @GetMapping("/add")
+    public String showFormAdd() {
+        return "users/add";
+    }
+
+    @PostMapping("/add")
+    public String saveAdd(@ModelAttribute("userRequest") UserRequest userRequest) {
+        System.out.println(userRequest.getName());
         return "redirect:/users";
     }
 }
